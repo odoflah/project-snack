18eb2fa17f04ece48ca2138cb2cc23f219ffa034
Alexandre Shinebourne <a.shinebourne@gmail.com>
2022-08-22 13:52:39 +0100

    now it's dynamic

--- gateway/main.go
+++ gateway/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
 	"strings"
@@ -32,7 +33,7 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 	url, _ := url.Parse(target)
 
 	// create the reverse proxy
-	// proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy := httputil.NewSingleHostReverseProxy(url)
 
 	// Update the headers to allow for SSL redirection
 	req.URL.Host = url.Host
@@ -43,12 +44,12 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 	fmt.Println("I'm here")
 
 	// Note that ServeHttp is non blocking and uses a go routine under the hood
-	// proxy.ServeHTTP(res, req)
+	proxy.ServeHTTP(res, req)
 	// TODO: make an http request here to test if I can make a request
-	_, err := http.Get("http://greeting:8002")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// _, err := http.Get("http://greeting:8002")
+	// if err != nil {
+	// log.Fatalln(err)
+	// }
 }
 
 func service(url string) string {
