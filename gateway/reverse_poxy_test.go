16c04dd6d468a21ba84a158bcae8b3ae3469ba4e
Alexandre Shinebourne <a.shinebourne@gmail.com>
2022-09-01 13:11:32 +0100

    added test files for gateway

---
+++ gateway/reverse_poxy_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestServiceURLCreation(t *testing.T) {
+	// TODO
+}
+
+func TestInvalidServiceCall(t *testing.T) {
+	// TODO
+}
---
+++ gateway/route_protection_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestValidApiCall(t *testing.T) {
+	// TODO
+}
+ßß
+func TestValidInvalidCall(t *testing.T) {
+	// TODO
+}
