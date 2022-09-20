541ef862b4d42a0252086a8222696af8a228d538
Oisin O'Flaherty <oisinoflaherty@google.com>
2022-09-20 11:25:58 +0100

    update sql schema

--- backends/auth/init.sql
+++ backends/auth/init.sql
@@ -9,3 +9,22 @@ create table user_sessions (
   expiry timestamp,
   CONSTRAINT fk_session_user FOREIGN KEY (username) REFERENCES users(username)
 );
+
+create table snacks (
+  snackId serial NOT NULL,
+  snackName text,
+  snackDesc text,
+  snackCat text,
+  snackPic text,
+  healthScore int,
+  PRIMARY KEY (snackId)
+);
+
+create table snackSighting (
+  snack_id text,
+  CONSTRAINT fk_snack_id FOREIGN KEY (snack_id) REFERENCES snacks(snack_id),
+  sightTime dateTime,
+  sightLocation text,
+  sightEstDuration dateTime,
+  CONSTRAINT pk_sighting PRIMARY KEY (snack_id, sightTime, sightLocation)
+);
