3fe39d5150d06f143199e6860bb52cd0d93b6719
Oisin O'Flaherty <oisinoflaherty@google.com>
2022-09-20 14:33:16 +0100

    setup connection from snacktrack service to db with example function

--- backends/snacktrack/go.mod
+++ backends/snacktrack/go.mod
@@ -1,3 +1,7 @@
 module github.com/a-shine/app-template/snacktrack
 
 go 1.19
+
+require (
+	github.com/lib/pq v1.10.6 // indirect
+)
--- backends/snacktrack/main.go
+++ backends/snacktrack/main.go
@@ -3,13 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"database/sql"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
+// The "db" package level variable will hold the reference to our database instance
+var db *sql.DB
 
 func main() {
+	// Sleep to allow database process to start up...
+	time.Sleep(5 * time.Second)
+
+	// Create connection string based on environment (the production environment overwrites the development environment)
+	connectionString := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"))
+
+	// Open a connection to the database
+	var err error
+	db, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	// Verify connection to database
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":80", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is snacktrack!")
+	db.Query(` INSERT INTO snacks (snackname, snackdesc, snackcat, snackpic, healthscore)
+							VALUES ('test', 'test', 'test', 'test', 5)`)
+	result, err := db.Query("select * from snacks")
+	if err != nil{
+		fmt.Fprintln(w, err)
+	}else{
+		for result.Next() {
+			var id int
+			var name string
+			var desc string
+			var cat string
+			var image string
+			var score int
+
+			if err := result.Scan(&id, &name, &desc, &cat, &image, &score); err != nil {
+				fmt.Fprintln(w, err)
+			}
+			fmt.Fprintln(w, name)
+		}
+	}	
 }
