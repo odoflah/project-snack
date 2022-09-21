04bf462090d518dadbb919a4f11da4423c5cf263
Alexandre Shinebourne <a.shinebourne@gmail.com>
2022-09-21 11:06:03 +0100

    added inerface

--- frontend/src/app/snack-list/snack-list.component.ts
+++ frontend/src/app/snack-list/snack-list.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
+export interface Snack {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  location: string;
+  picture: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA: Snack[] = [
+  { name: 'Kit Kat', location: "Docs MK8-3", picture: 'H' },
+  { name: 'Orange Chocolate', location: "Docs MK8-3", picture: 'He' },
+  { name: 'Smoked Almonds', location: "Docs MK8-3", picture: 'Li' },
+  { name: 'Spinach', location: "Docs MK8-3", picture: 'Be' },
+  { name: 'Brocoli', location: "Docs MK8-3", picture: 'B' },
+  { name: 'Roast peppers', location: "Docs MK8-3", picture: 'C' },
+  { name: 'Water', location: "Docs MK8-3", picture: 'N' },
 ];
 
 
@@ -28,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class SnackListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'location', 'picture'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
